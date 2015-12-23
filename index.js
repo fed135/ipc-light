301df6d69190bffb85bfc4bac0151c4ffbdc6bb4
frederic charette <frederic.charette2@ubisoft.com>
2015-12-23 16:57:10 -0500

    Issue #1 - Exposing client, server + defaults

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
+/**
+ * Main
+ */
+
+'use strict';
+
+/* Requires ------------------------------------------------------------------*/
+
 var Client = require('./src/Client');
 var Server = require('./src/Server');
+var defaults = require('./src/defaults');
+
+/* Exports -------------------------------------------------------------------*/
 
 module.exports = {
+	/**
+	 * Helper function to create an IPC server
+	 */
 	createServer: function(requestHandler) {
 		var server = new Server();
 		server.handler = requestHandler;
 		return server;
 	},
+	/**
+	 * Helper function to create a client and listen to a server
+	 */ 
 	connect: function(config, callback) {
 		var client = new Client(config);
 		client.connect(callback);
 		return client;
-	}
+	},
+	Client: Client,
+	Server: Server,
+	defaults: defaults
 };
